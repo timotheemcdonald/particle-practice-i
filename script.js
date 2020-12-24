7f8007500a275daf76544d1b0c163f3e11f88312
timotheemcdonald <timotheemcdonald@gmail.com>
2020-12-23 23:46:34 -0500

    setting up

---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("canvas-one");
+const context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray;
