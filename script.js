443a2a47fff8bc0539bdf42ae90a39eac793e8f2
timotheemcdonald <timotheemcdonald@gmail.com>
2020-12-23 23:55:09 -0500

    setting up detection functions and draw particle functions

--- script.js
+++ script.js
@@ -4,3 +4,73 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let particlesArray;
+
+//get mouse position
+
+let mouse = {
+    x: null,
+    y: null,
+    radius: (canvas.height/80) * (canvas.width/80);
+}
+
+window.addEventListener('mousemove',
+function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+
+//create particle
+class Particle {
+    constructor(x, y, directionX, directionY, size, color){
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.color = color;
+    }
+    //method to draw individual particles
+    draw(){
+        context.beginPath();
+        context.arc(this.x, this.y, this.size, 0, Math.PI *2, false);
+        context.fillStyle = '#8C5523';
+        context.fill();
+    }
+
+    //check particle position, check mouse position, move the particle, draw the particle
+    update(){
+        //check if particle is still within canvas
+        if (this.x > canvas.width || this.x < 0 ){
+            this.directionX = -this.directionX;
+        }
+        if(this.y > canvas.height || this.y < 0){
+            this.directionY = -this.directionY;
+        }
+
+        //check collision detection - mouse position / particle position
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx*dx + dy*dy);
+        if (distance < mouse.radius + this.size){
+            if(mouse.x < this.x && this.x < canvas.width - this.size * 10){
+                this.x += 10;
+            }
+            if(mouse.x > this.x && this.x > this.size * 10){
+                this.x -= 10;
+            }
+            if(mouse.y < this.y && this.y < canvas.height - this.size * 10){
+                this.y += 10;
+            }
+            if(mouse.y > this.y && this.y > this.size * 10){
+                this.y -= 10;
+            }
+        }
+        //move particle
+        this.x += this.directionX;
+        this.y += this.directionY;
+        //draw particle
+        this.draw();
+    }
+}
+
+//create particle array
